4c0c3d2a5a61660f81954504c78a4c4f09849709
yashyadav99940-png <yashyadav99940@gmail.com>
2026-05-19 18:56:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('this is normal text')
+console.warn('This is warning')
+console.error('this is error')
+var a = 12 //intialisation nd declaration
+console.log(a);
+
+ var b = 20  //updation
+console.log(a);
+console.log('value of abc is ',a);
+
+//arithmatic operations
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a%b);
+
+// 
